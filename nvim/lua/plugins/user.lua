6cfcf8cb907b34c582d72f91383ff50a85ab8f88
eduardo <eduardgm201@gmail.com>
2025-05-25 13:59:03 -0400

    add vimwiki to neovim

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -84,4 +84,19 @@ return {
     end,
   },
   { "catppuccin/nvim" },
+  {
+    "vimwiki/vimwiki",
+    init = function()
+      vim.g.vimwiki_list = {
+        {
+          syntax = "markdown",
+          ext = ".md",
+          path = "/mnt/files/nextcloud/wiki/",
+          dairy_rel_path = "notes",
+        },
+      }
+      vim.g.vimwiki_global_ext = 0
+      vim.g.vimwiki_auto_header = 1
+    end,
+  },
 }
